8111b9825b8a7d879c2fa5a6e96f5aee4fbfe4d2
adhithyaarun <adhithya.arun@students.iiit.ac.in>
2018-04-06 00:33:59 +0530

    changes to the FlaskTutorial

--- FlaskTutorial/microblog/app/routes.py
+++ FlaskTutorial/microblog/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
 
@@ -22,6 +22,6 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}').format(form.username.data, form.remember_me.data)
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect('/index')
     return render_template('login.html', title = 'Sign In', form = form)
