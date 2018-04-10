417058793d615d0db30a9da92b951976e78ad79f
ShashwatNigam99 <meher.shashwat@students.iiit.ac.in>
2018-04-10 16:06:27 +0530

    avatars and profile page

--- FlaskTutorial/taskmanager/app/models.py
+++ FlaskTutorial/taskmanager/app/models.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
+from hashlib import md5
 
 user_boards = db.Table('user_boards',
                        db.Column('user_id', db.Integer,
@@ -17,12 +18,18 @@ class User(UserMixin, db.Model):
                          unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(150), index=True, unique=True, nullable=False)
+    about_me = db.Column(db.String(150))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
 
 
 class Board(db.Model):
